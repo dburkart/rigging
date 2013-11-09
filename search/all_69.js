f91bac966d6653bdda8ac939408aabe7dd3af3fa
Dana Burkart <dana.burkart@gmail.com>
2013-11-09 01:30:49 -0800

    Commiting documentation for a48872f8ad5e2f6459d40f686aa9f115c2e2678c

--- search/all_69.js
+++ search/all_69.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['init',['init',['../class_module.html#a15368ae239d3d44d481c1bd902b496bd',1,'Module']]]
+  ['init',['init',['../class_rigging.html#abf53b1e68e9febcf7bcaad1e874129ff',1,'Rigging\init()'],['../class_module.html#a15368ae239d3d44d481c1bd902b496bd',1,'Module\init()']]]
 ];
