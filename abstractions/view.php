96b3c2bef1132e1d4975714670bd7eed2ac58d84
Dana Burkart <dana.burkart@gmail.com>
2013-11-09 21:01:03 -0800

    Adds support for rendering layers directly in Views.
    Views now treat Layers as associative arrays, and gather any public properties
    that exist in a given layer.

--- abstractions/view.php
+++ abstractions/view.php
@@ -6,6 +6,47 @@
 
 class View extends Scurvy {
 
+	/**
+	 * View overrides Scurvy::set, so as to allow the passing of Layers through
+	 * (as associative arrays).
+	 *
+	 * \param $var the variable to set
+	 * \param $val the value to set $var to
+	 */
+	public function set( $var, $val ) {
+		$resolvedValue = $val;
+
+		if ( is_array( $val ) ) {
+			$resolvedValue = array();
+
+			foreach ( $val as $assocArray ) {
+				if ( is_a( $assocArray, 'Layer' ) ) {
+					$resolvedValue[] = $this->resolveLayerToAssocArray( $assocArray );
+				} else {
+					$resolvedValue[] = $assocArray;
+				}
+			}
+		}
+
+		parent::__call( 'set', array( $var, $resolvedValue ) );
+	}
+
+	private function resolveLayerToAssocArray( $layer ) {
+		$assocArray = array();
+		$reflect = new ReflectionObject( $layer );
+
+		$properties = $reflect->getProperties( 
+			ReflectionMethod::IS_PUBLIC
+		);
+
+		foreach ( $properties as $property ) {
+			$name = $property->getName();
+			$assocArray[$name] = $layer->$name;#$layer->$property;
+		}
+
+		return $assocArray;
+	}
+
 }
 
 /**
