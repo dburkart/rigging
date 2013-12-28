bc497d64099a345d518aefb10f13398161b6cc73
Dana Burkart <dana.burkart@gmail.com>
2013-12-28 12:03:04 -0800

    Commiting documentation for 50d19b02db6909956a674d6bf1ba58c40f454693

--- search/functions_5f.js
+++ search/functions_5f.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['_5f_5f_5fpost_5finit',['___POST_INIT',['../class_flow.html#a4a9f162fc14416a2a48b879f6d7b1b4c',1,'Flow']]],
-  ['_5f_5fconstruct',['__construct',['../class_dependency_injector.html#a7bf288c1816e8e2e5eddf613e9f40e20',1,'DependencyInjector\__construct()'],['../class_module.html#a9eacd1ee29256a992b3499fe11ff72da',1,'Module\__construct()']]]
+  ['_5f_5fconstruct',['__construct',['../class_dependency_injector.html#a7bf288c1816e8e2e5eddf613e9f40e20',1,'DependencyInjector\__construct()'],['../class_layer.html#a6220eaaf2e074969957e3c8da44b1cdc',1,'Layer\__construct()'],['../class_module.html#a9eacd1ee29256a992b3499fe11ff72da',1,'Module\__construct()']]]
 ];
--- search/variables_24.js
+++ search/variables_24.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['_24base_5fdir',['$base_dir',['../class_module.html#a45dbdf7cf3a317b175c92af3158103dd',1,'Module']]],
-  ['_24create',['$create',['../class_module.html#a5890874924c0885a34349c90df05e2c7',1,'Module']]],
+  ['_24base_5fdir',['$base_dir',['../class_layer.html#a9d88daa372fd3423e72c65b81f53e1a8',1,'Layer\$base_dir()'],['../class_module.html#a45dbdf7cf3a317b175c92af3158103dd',1,'Module\$base_dir()']]],
+  ['_24create',['$create',['../class_layer.html#ad734951cf51adcba966f9a622e3b1a75',1,'Layer\$create()'],['../class_module.html#a5890874924c0885a34349c90df05e2c7',1,'Module\$create()']]],
   ['_24view',['$view',['../class_module.html#a8fcea0413f2ce2330ce1caee3b402793',1,'Module']]]
 ];
